0b57f4e8178ed057e71668754a63fc4207aaadc7
Runkang10 <chen.runkang10@gmail.com>
2026-09-02 16:46:02 +0200

    feat: add header for translations

--- src/main/kotlin/io/github/runkang10/atomicfreeze/AtomicFreezeBootstrap.kt
+++ src/main/kotlin/io/github/runkang10/atomicfreeze/AtomicFreezeBootstrap.kt
@@ -5,6 +5,7 @@ import io.github.runkang10.atomicfreeze.configurations.DefaultTranslations
 import io.github.runkang10.atomicfreeze.services.PrefixedSender
 import io.github.runkang10.compactmono.configuration.LoggedConfiguration
 import io.github.runkang10.compactmono.services.ColoredLogger
+import io.github.runkang10.compactmono.utilities.strings
 import io.papermc.paper.plugin.bootstrap.BootstrapContext
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap
 import io.papermc.paper.plugin.bootstrap.PluginProviderContext
@@ -21,20 +22,27 @@ internal class AtomicFreezeBootstrap : PluginBootstrap {
 
         val pluginFolder = context.dataDirectory.toFile()
         settings = LoggedConfiguration(
-            pluginFolder.resolve("settings.conf"),
-            DefaultSettings::class,
-            DefaultSettings(),
-            ConfigurationOptions.defaults(),
-            null,
-            logger
+            file = pluginFolder.resolve("settings.conf"),
+            type = DefaultSettings::class,
+            default = DefaultSettings(),
+            options = ConfigurationOptions.defaults(),
+            migrations = null,
+            logger = logger
         )
         translations = LoggedConfiguration(
-            pluginFolder.resolve("translations.conf"),
-            DefaultTranslations::class,
-            DefaultTranslations(),
-            ConfigurationOptions.defaults(),
-            null,
-            logger
+            file = pluginFolder.resolve("translations.conf"),
+            type = DefaultTranslations::class,
+            default = DefaultTranslations(),
+            options = ConfigurationOptions.defaults()
+                .header(
+                    strings(
+                        "\n",
+                        "Welcome to AtomicFreeze translations file. Here you can change the message output for certain actions and commands.",
+                        "NOTE: PlaceholderAPI is NOT supported and some internal placeholders like <target> can only work in certain messages."
+                    )
+                ),
+            migrations = null,
+            logger = logger
         )
         settings.load()
         translations.load()
